95b582c9f828c3893e1b0fb9b9420fdc9314af88
Zach Nelson <143453908+ZNels0n@users.noreply.github.com>
2025-03-06 16:09:32 -0700

    added files

---
+++ Show_names.js
@@ -0,0 +1,46 @@
+let names = ['Albert Einstein', 'Bill Gates', 'Michael Jackson','Oprah Winfrey','Leonardo DiCaprio','Elon Musk','Serena Williams','Winston Churchill','Abraham Lincoln','Marilyn Monroe','David Beckham','Barack Obama','Emma Watson','Taylor Swift','Steve Jobs','Dwayne Johnson','Princess Diana' ,'Kobe Bryant' ,'Nikola Tesla' ,'Bruce Lee' ,'Michael Jordan','Meryl Streep','James Cameron'  ,'Usain Bolt','Muhammad Ali','Freddie Mercury','John F. Kennedy','Lady Gaga'];
+let familiarNames = [];
+let currentIndex = 0;
+
+
+function markFamiliar(isFamiliar) {
+    if (isFamiliar) {
+        familiarNames.push(names[currentIndex]);
+    }
+    currentIndex++;
+    if (familiarNames.length < 10) {
+        displayNextName();
+    } else {
+        endSurvey();
+    }
+}
+
+function Save_data(names_list){
+    const link = document.createElement("a");
+    var nameBlob = new Blob([names_list],{type:'text/plain'});
+    link.setAttribute('href', URL.createObjectURL(nameBlob));
+    link.setAttribute('download', 'Familiarnames.csv');
+    link.click();
+    URL.revokeObjectURL(link.href);
+}
+
+
+function endSurvey() {
+    document.getElementById("survey-container").style.display = "none";
+    document.getElementById("result").innerText = "Survey complete! Familiar names: " + familiarNames.join(", ");
+    Save_data(familiarNames.join(",\n"))
+}
+
+
+function displayNextName() {
+    if (currentIndex < names.length) {
+        document.getElementById("name-display").innerText = names[currentIndex];
+    } else {
+        endSurvey();
+    }
+}
+
+
+// Start the survey
+
+displayNextName();
